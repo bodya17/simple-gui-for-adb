dab2ee6c173fb00dd9e1a59a6324b44646f3db39
bodya17 <levantovychbogdan@gmail.com>
2018-05-23 01:25:16 +0300

    add ability to enter text

--- main.js
+++ main.js
@@ -1,36 +1,45 @@
-const express = require('express')
-const exec = require('child_process').exec
-const app = express()
+const express = require('express');
+const exec = require('child_process').exec;
+const app = express();
 
 app.use(express.static('static'));
 
 function map(x1, y1, x2, y2) {
-    const m = (y1 - y2) / (x1 - x2)
-    return x => m * (x - x1) + y1
+  const m = (y1 - y2) / (x1 - x2);
+  return x => m * (x - x1) + y1;
 }
 
-const transformY = map(0, 0, 700, 1280)
-const transformX = map(0, 0, 450, 720)
+const transformY = map(0, 0, 700, 1280);
+const transformX = map(0, 0, 450, 720);
 
 app.get('/tap', (req, res) => {
-    const x = +req.query.x
-    const y = +req.query.y
-    exec(`adb shell input tap ${transformX(x)} ${transformY(y)}`)
-    res.json({status: 'ok'})
-})
+  const x = +req.query.x;
+  const y = +req.query.y;
+  exec(`adb shell input tap ${transformX(x)} ${transformY(y)}`);
+  res.json({ status: 'ok' });
+});
 
 app.get('/back', (req, res) => {
-    exec('adb shell input keyevent 4')
-    res.json({status: 'ok'})
-})
+  exec('adb shell input keyevent 4');
+  res.json({ status: 'ok' });
+});
 
 app.get('/swipe', (req, res) => {
-    const x1 = +req.query.x1
-    const y1 = +req.query.y1
-    const x2 = +req.query.x2
-    const y2 = +req.query.y2
-    exec(`adb shell input swipe ${transformX(x1)} ${transformY(y1)} ${transformX(x2)} ${transformY(y2)}`)
-    res.json({status: 'ok'})
-})
+  const x1 = +req.query.x1;
+  const y1 = +req.query.y1;
+  const x2 = +req.query.x2;
+  const y2 = +req.query.y2;
+  exec(
+    `adb shell input swipe ${transformX(x1)} ${transformY(y1)} ${transformX(
+      x2
+    )} ${transformY(y2)}`
+  );
+  res.json({ status: 'ok' });
+});
 
-app.listen(3001, () => console.log('server listening on port 3001'))
+app.get('/input_text', (req, res) => {
+  exec(`adb shell input text "${req.query.text.replace(/ /g, '%s')}"`);
+  res.json({ status: 'ok' });
+});
+
+app.listen(3001, () => console.log('server listening on port 3001'));
--- static/main.js
+++ static/main.js
@@ -1,45 +1,55 @@
-const screen = document.querySelector('.screen')
-const backBtn = document.querySelector('#back')
+const screen = document.querySelector('.screen');
+const backBtn = document.querySelector('#back');
+const sendTextBtn = document.querySelector('#send_text');
+const textInput = document.querySelector('input');
 
 const coords = {
-    mousedown: {
-        x: 0,
-        y: 0
-    },
-    mouseup: {
-        x: 0,
-        y: 0
-    }
-}
+  mousedown: {
+    x: 0,
+    y: 0
+  },
+  mouseup: {
+    x: 0,
+    y: 0
+  }
+};
 
 screen.addEventListener('mousedown', function(e) {
-    console.log('mousedown')
-    coords.mousedown.x = e.offsetX
-    coords.mousedown.y = e.offsetY
-})
+  console.log('mousedown');
+  coords.mousedown.x = e.offsetX;
+  coords.mousedown.y = e.offsetY;
+});
 
 screen.addEventListener('mouseup', function(e) {
-    console.log('mouseup')
-    coords.mouseup.x = e.offsetX
-    coords.mouseup.y = e.offsetY
-})
+  console.log('mouseup');
+  coords.mouseup.x = e.offsetX;
+  coords.mouseup.y = e.offsetY;
+});
 
 screen.addEventListener('click', function(e) {
-    if (coords.mousedown.x == coords.mouseup.x && coords.mousedown.y == coords.mouseup.y)
-        fetch(`tap?x=${e.offsetX}&y=${e.offsetY}`)
-            .then(b => b.json())
-            .then(console.log)
-    else {
-        const x1 = coords.mousedown.x
-        const y1 = coords.mousedown.y
-        const x2 = coords.mouseup.x
-        const y2 = coords.mouseup.y
-        fetch(`swipe?x1=${x1}&y1=${y1}&x2=${x2}&y2=${y2}`)
-    }
-})
+  if (
+    coords.mousedown.x == coords.mouseup.x &&
+    coords.mousedown.y == coords.mouseup.y
+  )
+    fetch(`tap?x=${e.offsetX}&y=${e.offsetY}`)
+      .then(b => b.json())
+      .then(console.log);
+  else {
+    const x1 = coords.mousedown.x;
+    const y1 = coords.mousedown.y;
+    const x2 = coords.mouseup.x;
+    const y2 = coords.mouseup.y;
+    fetch(`swipe?x1=${x1}&y1=${y1}&x2=${x2}&y2=${y2}`);
+  }
+});
 
 backBtn.addEventListener('click', function(e) {
-    fetch('back')
-        .then(b => b.json())
-        .then(console.log)
-})
+  fetch('back')
+    .then(b => b.json())
+    .then(console.log);
+});
+
+sendTextBtn.addEventListener('click', () => {
+  const text = textInput.value;
+  fetch(`input_text?text=${text}`);
+});
